bec24106b6ab2cdda340083e244c337f57470cb3
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-03-31 17:48:36 +0300

    DatabasePopulateService.java: add method's for write date in tables with using prepared statement SQLFileReader.java: add method's for read populate_db.sql

--- src/main/java/org/example/DatabasePopulateService.java
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -1,15 +1,115 @@
 package org.example;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DatabasePopulateService {
 
     private static final String populate_dbPath = "sql\\populate_db.sql";
     public static void main(String[] args) throws IOException, SQLException {
-        SQLFileReader sqlFileReader = new SQLFileReader();
-        String sql = sqlFileReader.readFile(populate_dbPath);
 
-        Database.sendSQLExecuteUpdate(sql);
+        //open connection
+        Database database = Database.getInstance();
+        Connection conn = database.getConnection();
+
+        //create prepared statement from client table
+        PreparedStatement statementClient = conn.prepareStatement("INSERT INTO client (name) VALUES (?)");
+        List<String> clientTableData = new SQLFileReader().readDataForClientTable(populate_dbPath);
+
+        for (String name: clientTableData) {
+            statementClient.setString(1, name);
+            statementClient.addBatch();
+        }
+        statementClient.executeBatch();
+        statementClient.close();
+
+
+        //create prepared statement from worker table
+        PreparedStatement statementWorker = conn.prepareStatement("INSERT INTO worker (name, birthday, level, salary) VALUES (?,?,?,?)");
+        List<String> workerTableData = new SQLFileReader().readDataForWorkerTable(populate_dbPath);
+
+        String workerName;
+        String workerBirthday;
+        String workerLevel;
+        String workerSalary;
+        List<String> workerLine = new ArrayList<>();
+
+        for (int i = 0; i < workerTableData.size(); i++) {
+            workerLine.add(workerTableData.get(i));
+
+                String[] array = workerLine.get(i).split("\\s+,");
+
+                workerName = array[0].trim();
+                workerBirthday = array[1].trim();
+                workerLevel = array[2].trim();
+                workerSalary = array[3].trim();
+
+                statementWorker.setString(1,workerName);
+                statementWorker.setDate(2, Date.valueOf(workerBirthday));
+                statementWorker.setString(3,workerLevel);
+                statementWorker.setInt(4, Integer.parseInt(workerSalary));
+                statementWorker.addBatch();
+                statementWorker.executeBatch();
+        }
+        statementWorker.close();
+
+
+        //create prepared statement from project table
+        PreparedStatement statementProject = conn.prepareStatement("INSERT INTO project (client_ID, start_date, finish_date) VALUES (?,?,?)");
+        List<String> projectTableData = new SQLFileReader().readDataForProjectTable(populate_dbPath);
+
+        String clientID;
+        String startDate;
+        String finishDate;
+        List<String> projectLine = new ArrayList<>();
+        for (int i = 0; i < projectTableData.size(); i++) {
+        projectLine.add(projectTableData.get(i));
+
+            String[] projectArray = projectLine.get(i).split(",");
+            clientID = projectArray[0].trim();
+            startDate = projectArray[1].trim();
+            finishDate = projectArray[2].trim();
+
+        statementProject.setInt(1, Integer.parseInt(clientID));
+        statementProject.setDate(2, Date.valueOf(startDate));
+        statementProject.setDate(3, Date.valueOf(finishDate));
+        statementProject.addBatch();
+        statementProject.executeBatch();
+        }
+
+        statementProject.close();
+
+
+
+        //create prepared statement from project_worker table
+        PreparedStatement statementPW = conn.prepareStatement("INSERT INTO project_worker (project_ID, worker_ID) VALUES (?,?)");
+        List<String> projectWorkerTableData = new SQLFileReader().readDataForProjectWorkerTable(populate_dbPath);
+
+        String pID;
+        String wID;
+
+        List<String> projectWorkerLine = new ArrayList<>();
+        for (int i = 0; i < projectWorkerTableData.size(); i++){
+            projectWorkerLine.add(projectWorkerTableData.get(i));
+
+            String[] projectWorkerArray = projectWorkerLine.get(i).split(" \\s+");
+            pID = projectWorkerArray[0].trim();
+            wID = projectWorkerArray[1].trim();
+
+
+            statementPW.setInt(1, Integer.parseInt(pID));
+            statementPW.setInt(2, Integer.parseInt(wID));
+            statementPW.addBatch();
+            statementPW.executeBatch();
+
+        }
+            statementPW.close();
+
+//close connection
+        database.closeConnection();
+
     }
 }
--- src/main/java/org/example/SQLFileReader.java
+++ src/main/java/org/example/SQLFileReader.java
@@ -3,6 +3,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLFileReader {
 
@@ -13,7 +15,7 @@ public class SQLFileReader {
         StringBuilder stringBuilder = new StringBuilder();
         String line;
 //read file
-        while ((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             stringBuilder.append(line);
             stringBuilder.append("\n");
         }
@@ -21,4 +23,104 @@ public class SQLFileReader {
 
         return stringBuilder.toString();
     }
+
+
+    public List<String> readDataForClientTable(String filePath) throws IOException {
+        //create buffer reader for read file
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        String line;
+        List<String> clientList = new ArrayList<>();
+//read file
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("INSERT INTO client")) {
+                reader.readLine();               //skip line - (name)
+                reader.readLine();               //skip line - VALUES
+                while (!line.contains(";")) {
+                    line = reader.readLine();
+                    String clientName = line.replaceAll("[^A-Za-z0-9]", "");
+                    clientList.add(clientName);
+                }
+            }
+        }
+        reader.close();
+
+        return clientList;
+    }
+
+    public List<String> readDataForWorkerTable(String filePath) throws IOException {
+        //create buffer reader for read file
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        List<String> workerDataList = new ArrayList<>();
+//read file
+        while ((line = reader.readLine()) != null){
+            if (line.equals("INSERT INTO worker")){
+                line = reader.readLine();               //skip line - (name, birthday, level, salary)
+                line = reader.readLine();               //skip line - VALUES
+                while (!line.contains(";")){
+                    line = reader.readLine();
+                    String workerData = line.replaceAll("[^A-Za-z0-9,-]", " ");
+                    workerData.split("\\s+");
+                    workerDataList.add(workerData);
+                }
+            }
+        }
+
+        reader.close();
+
+        return workerDataList;
+    }
+
+    public List<String> readDataForProjectWorkerTable(String filePath) throws IOException {
+        //create buffer reader for read file
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        String line;
+        List<String> projectWorkerList = new ArrayList<>();
+//read file
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("INSERT INTO project_worker")) {
+                reader.readLine();               //skip line - (project_ID, worker_ID)
+                reader.readLine();               //skip line - VALUES
+                while (!line.contains(";")){
+                    line = reader.readLine();
+                    String projectWorkerData = line.replaceAll("[^0-9]", " ");
+                    projectWorkerData.split("\\s+");
+                    projectWorkerList.add(projectWorkerData);
+                }
+            }
+        }
+        reader.close();
+
+        return projectWorkerList;
+    }
+
+    public List<String> readDataForProjectTable(String filePath) throws IOException {
+        //create buffer reader for read file
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        List<String> workerDataList = new ArrayList<>();
+//read file
+        while ((line = reader.readLine()) != null){
+            if (line.equals("INSERT INTO project")){
+                line = reader.readLine();               //skip line - (name, birthday, level, salary)
+                line = reader.readLine();               //skip line - VALUES
+                while (!line.contains(";")){
+                    line = reader.readLine();
+                    String workerData = line.replaceAll("[^A-Za-z0-9,-]", " ");
+                    workerData.split("\\s+");
+                    workerDataList.add(workerData);
+                }
+            }
+        }
+
+        reader.close();
+
+        return workerDataList;
+    }
+
+
+
+
 }
